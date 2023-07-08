9a0ec4e33a335dc6b6dee98aeea1e36a263f5723
Alex Chi <iskyzh@gmail.com>
2023-07-08 16:26:23 -0400

    switch to astro, new design
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	experimental: {
+		redirects: true
+	},
+	redirects: {
+		'/pages/about': '/about',
+	},
+	site: 'https://www.skyzh.dev',
+	integrations: [mdx(), sitemap()],
+	output: 'static',
+	markdown: {
+		remarkPlugins: [
+			'remark-math',
+		],
+		rehypePlugins: [
+			['rehype-katex', {
+				// Katex plugin options
+			}]
+		]
+	}
+});
---
+++ src/content/blog/2022-06-18-rusttalk-rust-database.md
@@ -0,0 +1,12 @@
+---
+title: "RustTalk 第四期：与迟先生聊 Rust 与数据库系统"
+pubDate: "2022-06-18T10:00:00+08:00"
+tags: ["Rust", "数据库", "Podcast", "RustTalk"]
+description: "RustTalk 第四期：与迟先生聊 Rust 与数据库系统"
+external: true
+lang: 'zh-cn'
+---
+
+感谢西瓜哥哥的邀请！在今年六月份，我参加了 RustTalk 第四期的录制，闲聊数据库系统。
+
+链接：[https://rusttalk.github.io/podcast/004/](https://rusttalk.github.io/podcast/004/)
